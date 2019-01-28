815babc9227fe8f01723c70d6724c86f9ed92412
glebouch <garanceleboucher@gmail.com>
2019-01-28 19:08:24 +0100

    ui

---
+++ main.c
@@ -0,0 +1,19 @@
+
+int ft_min_int(int a, int b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+int algo(t_info *infos)
+{
+	int i = 0;
+	int nb_path_max = 0;
+
+	while (i < info->nb_of_box)
+	{
+		if (info->data[i]->commands != 0)
+			nb_path_max = (nb_path_max == 0) ? info->data[i]->nb_of_links : ft_min
+	}
+}
